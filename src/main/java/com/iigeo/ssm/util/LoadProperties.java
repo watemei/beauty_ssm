66872a27858d51ca93904dd8aeec3221fe939170
xiubing.wang <xiubing.wang@sifude.com>
2016-11-19 14:32:54 +0800

    修改：使用nio-file watchService实现属性文件修改

--- src/main/java/com/iigeo/ssm/util/LoadProperties.java
+++ src/main/java/com/iigeo/ssm/util/LoadProperties.java
@@ -2,8 +2,6 @@ package com.iigeo.ssm.util;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,39 +10,47 @@ import java.util.Map;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+
 public class LoadProperties {
 	protected static Logger LOG = LoggerFactory.getLogger(LoadProperties.class);
-	
 	private List<File> propertiesFiles = new ArrayList<File>();
 	private Map<String,Object> propertiesCache = new HashMap<String,Object>();
-
-	public void loadProperties(PropertiesHolder propertiesHolder, PropertiesWatcher propertiesWatcher){
+	private ExecutorService cachedThreadPool = Executors.newFixedThreadPool(1);  
+	public void loadProperties(PropertiesHolder propertiesHolder){
 		//遍历默认class路径下的。properties文件
-		String clazPath = Thread.currentThread().getContextClassLoader().getResource("").toString();
+		String clazPath = String.format("%s/WEB-INF/classes", System.getProperty("web.root"));//Class.class.getClass().getResource("/").getPath();
 		getPropertiesFiles(clazPath);
 		for(File propertiesFile: propertiesFiles){
 			setValueInMapFormFile(propertiesFile); 
 		}
 		propertiesHolder.setPropertiesCache(propertiesCache); 
-		propertiesWatcher.setPropertiesCache(propertiesCache);
-		propertiesWatcher.setLoadProperties(this);
+		FileWatchTask fileWatchTask = new FileWatchTask(clazPath, this);
+		
+        cachedThreadPool.execute(fileWatchTask);
+		LOG.info("LoadProperties file readed finish.");
 	}
 
 	private void setValueInMapFormFile(File propertiesFile) {
+		LOG.info("properties file read start. {}", propertiesFile.getName());
 		try {
 			InputStream in = new FileInputStream(propertiesFile);
 			ResourceBundle propertyResourceBundle = new PropertyResourceBundle(in);
 			Set<String> keys = propertyResourceBundle.keySet();
 			for(String key : keys){
 				propertiesCache.put(key, propertyResourceBundle.getObject(key));
+				LOG.debug(String.format("properties key value.[%s]", propertyResourceBundle.getObject(key)));
 			}
 		} catch (Exception e) {
 			LOG.error("properties file read fail.", e);
 		}
+		LOG.info("properties file read end. {}", propertiesFile.getName());
 	}
 
 	public void loadPropertiesFileChange(File file){
