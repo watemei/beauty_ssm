bf34b8f0192ee998ed882d74a445a447020903a2
xiubing.wang <xiubing.wang@sifude.com>
2016-11-19 14:32:54 +0800

    修改：使用nio-file watchService实现属性文件修改

--- src/main/java/com/iigeo/ssm/util/PropertiesHandler.java
+++ src/main/java/com/iigeo/ssm/util/PropertiesHandler.java
@@ -5,26 +5,15 @@ import org.slf4j.LoggerFactory;
 
 public class PropertiesHandler {
 	protected static Logger LOG = LoggerFactory.getLogger(PropertiesHandler.class);
-	
+
 	PropertiesHolder propertiesHolder = new PropertiesHolder();
-	PropertiesWatcher propertiesWatcher = new PropertiesWatcher();
 	LoadProperties loadProperties = new LoadProperties();
-	
-	public PropertiesHandler(){
-		loadProperties.loadProperties(propertiesHolder, propertiesWatcher);
+
+	public PropertiesHandler() {
+		loadProperties.loadProperties(propertiesHolder);
 	}
-	
+
 	public Object getValueByKey(String key) {
-		if(propertiesHolder.contains(key)){
-			return propertiesHolder.getValueByKey(key);
-		}else{
-			try {
-				loadProperties.loadProperties(propertiesHolder, propertiesWatcher);
-				return propertiesHolder.getValueByKey(key);
-			} catch (Exception e) {
-				LOG.error("读取默认路径下的properties文件失败", e);
-				return null;
-			}
-		}
+		return propertiesHolder.getValueByKey(key);
 	}
 }
--- src/main/java/com/iigeo/ssm/util/SystemUtil.java
+++ src/main/java/com/iigeo/ssm/util/SystemUtil.java
@@ -2,10 +2,27 @@ package com.iigeo.ssm.util;
 
 public class SystemUtil {
 	private PropertiesHandler propertiesHandler = new PropertiesHandler();
-	
+
 	public String getValueByKey(String key) {
-		propertiesHandler.getValueByKey(key);
-		return "";
+		return (String) propertiesHandler.getValueByKey(key);
 	}
-	
+
+	public static void main(String[] args) {
+		SystemUtil systemUtil = new SystemUtil();
+		System.out.println(systemUtil.getValueByKey("redis.pool.maxTotal"));
+		Thread thread1 = systemUtil.new MyThread();
+		thread1.run();
+
+	}
+
+	class MyThread extends Thread {
+
+		@Override
+		public void run() {
+			SystemUtil systemUtil = new SystemUtil();
+			System.out.println(this.getName() + "线程：" + systemUtil.getValueByKey("redis.pool.maxTotal"));
+		}
+
+	}
+
 }
